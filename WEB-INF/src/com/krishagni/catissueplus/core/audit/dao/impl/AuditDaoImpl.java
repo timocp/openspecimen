1b7a0fcc6117a33f1d5fa8686c153c6a40127d2b
marwahan <nitesh.marwaha@krishagni.com>
2015-08-19 08:39:56 +0530

    Incorporated indentation

--- WEB-INF/src/com/krishagni/catissueplus/core/audit/dao/impl/AuditDaoImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/audit/dao/impl/AuditDaoImpl.java
@@ -23,7 +23,7 @@ public class AuditDaoImpl extends AbstractDao<CustomRevisionEntity> implements A
 	public CustomRevisionEntity getEntityCreatedDetails(String auditTable, Long entityId) {
 		String sql = String.format(GET_ENTITY_CREATED_DETAILS_SQL, auditTable);
 		Query query = sessionFactory.getCurrentSession().createSQLQuery(sql)
-			.setLong("entityId", entityId);
+				.setLong("entityId", entityId);
 
 		List<Object[]> rows = query.list();
 		return rows.size() > 0 ? getRevisionEntity(rows.iterator().next()) : null;
@@ -38,7 +38,7 @@ public class AuditDaoImpl extends AbstractDao<CustomRevisionEntity> implements A
 		}
 		String sql = String.format(GET_ENTITY_LAST_MODIFIED_DETAILS_SQL, auditTable, whereClause);
 		Query query = sessionFactory.getCurrentSession().createSQLQuery(sql)
-			.setLong("entityId", entityId);
+				.setLong("entityId", entityId);
 
 		if (rev != null) {
 			query.setLong("rev", rev);
@@ -74,8 +74,8 @@ public class AuditDaoImpl extends AbstractDao<CustomRevisionEntity> implements A
 	public Long getRevision(String auditTable, Long entityId) {
 		String sql = String.format(GET_REVISION_ID_SQL, auditTable);
 		return Long.valueOf((sessionFactory.getCurrentSession()
-			.createSQLQuery(sql)
-			.setLong("entityId", entityId).list().get(0).toString()));
+				.createSQLQuery(sql)
+				.setLong("entityId", entityId).list().get(0).toString()));
 	}
 	
 	@Override
@@ -88,8 +88,8 @@ public class AuditDaoImpl extends AbstractDao<CustomRevisionEntity> implements A
 		
 		String sql = String.format(GET_SUBJECT_ROLE_LAST_MODIFIED_SQL, whereClause);
 		Query query = sessionFactory.getCurrentSession()
-				.createSQLQuery(sql)
-				.setLong("dsoId", dsoId);
+					.createSQLQuery(sql)
+					.setLong("dsoId", dsoId);
 		
 		if(rev != null) {
 			query.setLong("rev", rev);
