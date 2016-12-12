421bfa0481effcbc1c93512a9bb0ac2786f817cd
Vinayak Pawar <vinayakapawar@gmail.com>
2016-12-12 09:07:04 +0530

    OPSMN-3547: Fixed consent statement service bean name.
    Grouped public and private methods in consent statement DAO. All
    public methods appear before private methods...

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/repository/impl/ConsentStatementDaoImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/repository/impl/ConsentStatementDaoImpl.java
@@ -30,32 +30,32 @@ public class ConsentStatementDaoImpl extends AbstractDao<ConsentStatement> imple
 			.list();
 	}
 	
-	private Criteria getStatementListCriteria(ConsentStatementListCriteria listCrit) {
-		Criteria query = getCurrentSession().createCriteria(ConsentStatement.class);
-		addCodeRestriction(query, listCrit.code());
-		addStatementRestriction(query, listCrit.query());
-		return query;
-	}
-	
-	private void addCodeRestriction(Criteria query, String code) {
-		if (StringUtils.isNotBlank(code)) {
-			query.add(Restrictions.ilike("code", code, MatchMode.ANYWHERE));
-		}
-	}
-	
-	private void addStatementRestriction(Criteria query, String code) {
-		if (StringUtils.isNotBlank(code)) {
-			query.add(Restrictions.ilike("statement", code, MatchMode.ANYWHERE));
-		}
-	}
-	
 	@Override
 	public ConsentStatement getByCode(String code) {
 		return (ConsentStatement)getCurrentSession().getNamedQuery(GET_BY_CODE)
 			.setString("code", code)
 			.uniqueResult();
 	}
-	
+
+	private Criteria getStatementListCriteria(ConsentStatementListCriteria listCrit) {
+		Criteria query = getCurrentSession().createCriteria(ConsentStatement.class);
+		addCodeRestriction(query, listCrit.code());
+		addStatementRestriction(query, listCrit.query());
+		return query;
+	}
+
+	private void addCodeRestriction(Criteria query, String code) {
+		if (StringUtils.isNotBlank(code)) {
+			query.add(Restrictions.ilike("code", code, MatchMode.ANYWHERE));
+		}
+	}
+
+	private void addStatementRestriction(Criteria query, String code) {
+		if (StringUtils.isNotBlank(code)) {
+			query.add(Restrictions.ilike("statement", code, MatchMode.ANYWHERE));
+		}
+	}
+
 	private static final String FQN = ConsentStatement.class.getName();
 	
 	private static final String GET_BY_CODE = FQN + ".getByCode";
