14143dd2ce2f1bf7bc297d327c8c8d999ff048c5
vinaylonushte <vinayak.lonushte@krishagni.com>
2016-12-26 13:17:19 +0530

    OPSMN-3566: Hid radio button for matched participant which is registered to same cp   to avoid 'Participant is already registered to same cp' error.
    1. Populate registered CPs detail on demand, as matching participants API
       is invoked from multiple places where registered CPs detail is not used ...
    
    2. Added getAccessibleCprs method to get accessible cprs
    
    3. Used HTML table to display matching participant instead of div element

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/CollectionProtocolRegistrationServiceImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/CollectionProtocolRegistrationServiceImpl.java
@@ -887,20 +887,11 @@ public class CollectionProtocolRegistrationServiceImpl implements CollectionProt
 			return Collections.emptyList();
 		}
 
-		return participant.getCprs().stream()
-			.filter(otherCpr -> {
-				if (otherCpr.equals(cpr)) {
-					return true;
-				}
-
-				try {
-					AccessCtrlMgr.getInstance().ensureReadCprRights(otherCpr);
-					return true;
-				} catch (OpenSpecimenException e) {
-					return false;
-				}
-			})
-			.collect(Collectors.toList());
+		return AccessCtrlMgr.getInstance().getAccessibleCprs(
+			participant.getCprs().stream()
+				.filter(otherCpr -> !otherCpr.equals(cpr))
+				.collect(Collectors.toList())
+		);
 	}
 
 	private CollectionProtocolRegistrationDetail registerParticipant(BulkRegistrationsDetail bulkRegDetail, Date regDate, boolean checkPermission) {
